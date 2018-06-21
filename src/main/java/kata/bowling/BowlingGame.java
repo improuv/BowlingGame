5ad05de47d7b17e2a34695a079cbe52a54e59acd
Markus Langenegger <mlg@mlangenegger.com>
2018-06-21 07:16:09 +0200

    initial checkin

---
+++ src/main/java/kata/bowling/BowlingGame.java
@@ -0,0 +1,15 @@
+package kata.bowling;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class BowlingGame {
+
+	@Test
+	public void test() {
+		assertThat("1", is("1"));
+	}
+
+}
---
+++ src/main/java/kata/bowling/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author none
+ *
+ */
+package kata.bowling;
